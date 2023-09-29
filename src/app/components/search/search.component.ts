b7c226edd7d0adb4d59db5b694953819d59a0bac
Ian Montes <fotoianmontes@gmail.com>
2023-09-29 11:24:28 +0100

    datalist created with every bus stop on LPA

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import lineData from '../../../assets/data.json';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
+  dataset: string[];
 
+  fillDataSet(dataset: string[], element: any) {
+    element[1].forEach((e: string) => {
+      if (!dataset.includes(e[0])) {
+        dataset.push(e[0]);
+      }
+    });
+  }
+  constructor() {
+    this.dataset = [];
+    lineData.lines.forEach((element: any) => {
+      this.fillDataSet(this.dataset, element);
+    });
+    console.log(this.dataset);
+  }
 }
